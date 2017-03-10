2baf103ce21cdda389281b04522c418d442de57d
Ian McNaughton <IanMcNaugh@gmail.com>
2017-03-09 16:25:09 -0800

    flushed out the candidateDao

--- CountMeUp/src/main/scala/com/bbc/countMeUp/dao/impl/InMemoryCandidateDao.scala
+++ CountMeUp/src/main/scala/com/bbc/countMeUp/dao/impl/InMemoryCandidateDao.scala
@@ -5,17 +5,35 @@ import java.util.UUID
 import com.bbc.countMeUp.dao.CandidateDao
 import com.bbc.countMeUp.model.Candidate
 
+import scala.collection.mutable
+
 trait InMemoryCandidateDao extends CandidateDao{
   override def candidateDao = new InMemCandidateDao
 
+  var candidates: collection.mutable.Map[UUID, Candidate] = new mutable.HashMap[UUID, Candidate]
+
   class InMemCandidateDao extends CandidateDao {
-    override def create(model: Candidate): UUID = ???
+    override def create(model: Candidate): UUID = {
+      candidates.put(model.id, model) match {
+        case None => model.id
+        case _ => throw new Exception
+      }
+    }
 
-    override def read(id: UUID): Option[Candidate] = ???
+    override def read(id: UUID): Option[Candidate] = {
+      candidates.get(id)
+    }
 
-    override def update(model: Candidate): Candidate = ???
+    override def update(model: Candidate): Candidate = {
+      candidates.put(model.id, model) match {
+        case None => throw new Exception
+        case e: Some[Candidate] => e.get
+      }
+    }
 
-    override def delete(id: UUID): Unit = ???
+    override def delete(id: UUID): Unit = {
+      candidates.remove(id)
+    }
   }
 
 }
