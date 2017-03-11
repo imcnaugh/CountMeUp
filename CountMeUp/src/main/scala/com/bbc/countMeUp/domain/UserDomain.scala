6449b5eb8a820a5c0cffa0113a43b6760d56fa66
Ian McNaughton <IanMcNaugh@gmail.com>
2017-03-10 19:20:44 -0800

    filled in the voting method in user dao

--- CountMeUp/src/main/scala/com/bbc/countMeUp/domain/UserDomain.scala
+++ CountMeUp/src/main/scala/com/bbc/countMeUp/domain/UserDomain.scala
@@ -4,7 +4,7 @@ import java.util.UUID
 
 import com.bbc.countMeUp.dao.{ElectionDao, UserDao, VoteDao}
 import com.bbc.countMeUp.dao.util.IdUtils
-import com.bbc.countMeUp.model.User
+import com.bbc.countMeUp.model.{User, Vote}
 
 class UserDomain {
   this: UserDao with VoteDao with ElectionDao =>
@@ -24,5 +24,38 @@ class UserDomain {
       case u: Some[User] => u.get
       case _ => throw new Exception
     }
+  }
+
+  //TODO throw better exceptions
+  @throws(classOf[Exception])
+  def voteInElection(
+                      userId: UUID,
+                      electionId: UUID,
+                      candidateId: UUID): Vote = {
+    val election = electionDao.read(electionId).getOrElse(throw new Exception)
+    val userVoteCountInElection = voteDao.getVoteCountForElectionAndUser(electionId, userId)
+
+    //validate user exists
+    if(userDao.read(userId) == None){
+      throw new Exception
+    }
+    //Election not found
+    if(election.maxVotesPerUser <= userVoteCountInElection){
+      throw new Exception
+    }
+    // Candidate is not in this election
+    if(election.candidates.find(c => c.id == candidateId) == None){
+      throw new Exception
+    }
+
+    val newVote = Vote(
+      id = IdUtils.uniqueId(voteDao.read),
+      userId = userId,
+      electionId = electionId,
+      candidateId = candidateId
+    )
+
+    voteDao.create(newVote)
+    newVote
   }
 }
