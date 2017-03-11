5407a2bac6599cb715ba3fde7ea50aa2c33b6d4a
Ian McNaughton <IanMcNaugh@gmail.com>
2017-03-10 19:38:54 -0800

    filled in the candidate domain

--- CountMeUp/src/main/scala/com/bbc/countMeUp/domain/CandidateDomain.scala
+++ CountMeUp/src/main/scala/com/bbc/countMeUp/domain/CandidateDomain.scala
@@ -3,15 +3,28 @@ package com.bbc.countMeUp.domain
 import java.util.UUID
 
 import com.bbc.countMeUp.dao.CandidateDao
+import com.bbc.countMeUp.domain.util.IdUtils
 import com.bbc.countMeUp.model.Candidate
 
 class CandidateDomain {
   this: CandidateDao =>
 
-  def addCandidate(name: String): Candidate = ???
+  def addCandidate(name: String): Candidate = {
+    val newCandidate = Candidate(
+      IdUtils.uniqueId(candidateDao.read),
+      name
+    )
+    candidateDao.create(newCandidate)
+    newCandidate
+  }
 
   //TODO throw better exceptions
   @throws(classOf[Exception])
-  def getCandidate(id: UUID): Candidate = ???
+  def getCandidate(id: UUID): Candidate = {
+    candidateDao.read(id) match {
+      case c: Some[Candidate] => c.get
+      case _ => throw new Exception
+    }
+  }
 
 }
